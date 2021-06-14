51c2e88fe6dc0363165bc0c60c72120706f08e37
Shawon Saha <shawondroid@gmail.com>
2021-06-15 00:27:29 +0600

    Birthday Reminder Complete Project This application will show the people who have birthday today. You can clear the list by pressing clear all button

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
-import React, { useState } from 'react';
-import data from './data';
-import List from './List';
+import React, { useState } from "react";
+import data from "./data";
+import List from "./List";
+
 function App() {
-  return <h2>reminder project setup</h2>;
+  const [people, setPeople] = useState(data);
+
+  const clearData = () => {
+    setPeople([]);
+  };
+  return (
+    <main>
+      <section className="container">
+        <h4>{people.length} people have birthday today</h4>
+        <List people={people} />
+        <button className="btn" onClick={() => clearData()}>
+          clear all
+        </button>
+      </section>
+    </main>
+  );
 }
 
 export default App;
